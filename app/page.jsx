20ede803653fd4da007eda08738dad14b47ccc07
luiza <luizaschettinii@gmail.com>
2025-03-04 10:47:42 -0300

    feat: adjustments and responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -8,10 +8,10 @@ return (
     <div className={styles.container}>
         <Header />
     <main className={styles.main}>
-    <div className={styles.left}>
-        <Card title="Presenteie quem você ama com nossas belas flores na caixa" content="Nada expressa melhor o seu carinho do que um presente cuidadosamente elaborado. Nossas flores na caixa são arranjadas com amor e atenção aos detalhes, proporcionando uma experiência única e memorável para quem as recebe."/>
-        <Card title="Presenteie quem você ama com nossa cesta de flores" content="Surpreenda e encante quem você ama com nossa exclusiva cesta de flores. Cada cesta é cuidadosamente arrumada, contendo não apenas flores deslumbrantes, mas também um hidratante corporal de qualidade, um vinho selecionado e deliciosos biscoitos." />
-        <Card title="Presenteie quem você ama com nosso buquê de flores e caixa de chocolates" content="Surpreenda e encante com a combinação perfeita de um buquê de flores deslumbrantes e uma deliciosa caixa de chocolates. Cada buquê é cuidadosamente arranjado para capturar a beleza e a essência das flores, enquanto a caixa de chocolates premium oferece uma experiência saborosa."/>
+    <div className={styles.article}>
+        <Card className={styles.card} title="Presenteie quem você ama com nossas belas flores na caixa" content="Nada expressa melhor o seu carinho do que um presente cuidadosamente elaborado. Nossas flores na caixa são arranjadas com amor e atenção aos detalhes, proporcionando uma experiência única e memorável para quem as recebe."/>
+        <Card className={styles.card} title="Presenteie quem você ama com nossa cesta de flores" content="Surpreenda e encante quem você ama com nossa exclusiva cesta de flores. Cada cesta é cuidadosamente arrumada, contendo não apenas flores deslumbrantes, mas também um hidratante corporal de qualidade, um vinho selecionado e deliciosos biscoitos." />
+        <Card className={styles.card} title="Presenteie quem você ama com nosso buquê de flores e caixa de chocolates" content="Surpreenda e encante com a combinação perfeita de um buquê de flores deslumbrantes e uma deliciosa caixa de chocolates. Cada buquê é cuidadosamente arranjado para capturar a beleza e a essência das flores, enquanto a caixa de chocolates premium oferece uma experiência saborosa."/>
         </div>
         <aside className={styles.aside}>
             <div className={styles.flower}>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.footer}>
-        <p>Lu´s Petals Ltda ME | Rua dos Sabiás - São Paulo - SP</p>
+        <p>Lu´s Petals Ltda ME | Rua dos Sabiás - SP</p>
       </div>
     )
 }
